c17f7bd96af615df65d2c642b66791cc1ceb2162
jjsub <jsamesub@gmail.com>
2015-07-09 11:43:39 -0500

    toggle width must be set as style value to the div

---
+++ app.js
@@ -0,0 +1,10 @@
+
+$('.toggle').toggles({
+	drag: true,
+	text:{
+		on: "", // text for the ON position
+		off:""  // and off
+		},
+	width: "50px" // width used if not set in css
+	// if this is set to 'select' then the select style toggle will be used
+});
